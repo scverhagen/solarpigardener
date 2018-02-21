ba29981739709afe22aa12497a5bc8a4528935a3
Steve Verhagen <scverhagen@gmail.com>
2018-02-20 22:46:43 -0600

    More work on project

---
+++ src/daemon/gardener.cpp
@@ -0,0 +1,15 @@
+#include <ads1115.h>
+#define MY_PINBASE 2222
+
+#define ch0 MY_PINBASE + 0
+#define ch1 MY_PINBASE + 1
+#define ch2 MY_PINBASE + 2
+#define ch3 MY_PINBASE + 3
+
+void main(void)
+
+void main(void)
+{
+	ads1115Setup (MY_PINBASE, 0x48);
+	return 0;
+}  
