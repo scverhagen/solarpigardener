5cc29feda971b2a4a677a067489cc3473e5d8088
Steve Verhagen <scverhagen@gmail.com>
2019-07-07 12:46:31 -0500

    Restore watering duration to 5 minutes

--- app/__init__.py
+++ app/__init__.py
@@ -54,38 +54,6 @@ import app.gardener_fx as gardener_fx
 import app.gardener_controls as gardener_controls
 control_waterpump = gardener_controls.water_pump()
 
-def process_gardener_command(gc):
-    lcmd = gc.lower()
-    args = gc.split()
-    argc = len(args)
-    largs = lcmd.split()
-    
-    if largs[0] == 'water_pump_on':
-        control_waterpump.On()
-    elif largs[0] == "system_reboot":
-        print('Rebooting system...')
-        os.system("reboot")
-    elif largs[0] == "system_poweroff":
-        print('Powering system OFF...')
-        os.system("poweroff");
-    elif largs[0] == "ping":
-        print('ping')
-    elif largs[0] == "check_moisture":
-        #update_params_moisture_sensor()
-        print("Moisture param updated.")
-    elif largs[0] == 'force_water':
-        print("Forcing maintenance.")
-        #gardener_do_maint();
-    elif largs[0] == "kill_process":
-        print("Killing Process.")
-        sys.exit(1)
-    elif largs[0] == 'water_for':
-        if argc > 1:
-            duration = largs[1]
-            control_waterpump.water_for(int(duration))
-
-
-
 @celery.task()
 def do_water_for(secs):
     control_waterpump.water_for(secs)
@@ -98,7 +66,7 @@ def www_controls():
 
 @app.route('/pump5')
 def www_pump5():
-    do_water_for.delay(5)
+    do_water_for.delay(300)
     return render_template('pump5.html')
 
 @app.route('/favicon.ico')
