612cf1968d01db5a355f04c5bcfe09906ba1d4ec
Steve Verhagen <scverhagen@gmail.com>
2019-07-18 22:43:06 -0500

    finish login feature

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def get_ip():
     return IP
 
 
-from flask import Flask, request, send_file, render_template, redirect, url_for
+from flask import Flask, request, send_file, render_template, redirect, url_for, session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'garDener_debuG_seCret'
 
@@ -105,6 +105,7 @@ def www_config():
         
         # use old password if none specified:
         if g_settings.admin_password == '':
+            session.clear()
             g_settings.admin_password = old_password
 
         # save configuration
@@ -118,9 +119,18 @@ def www_login():
     g_settings = config.loadSettings()
     form = forms.LoginForm()
     if form.validate_on_submit():
+        if form.password.data == g_settings.admin_password:
+            session['logged_in'] = True
+            print(form.password)
         return redirect( url_for('www_root') )
     
     return render_template('login.html', form=form)
+
+@app.route('/logoff', methods=["GET"])
+def www_logoff():
+    session.clear()
+    
+    return redirect( url_for('www_root') )
 
 #needs to be at end of file:
 if __name__ == '__main__':
