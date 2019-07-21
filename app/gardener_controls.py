da1c5a0e5801fe2309b5d7fed8a53fc3838e8334
Steve Verhagen <scverhagen@gmail.com>
2019-07-21 14:23:04 -0500

    fix gpio pin type error

--- app/gardener_controls.py
+++ app/gardener_controls.py
@@ -22,34 +22,34 @@ factory = PiGPIOFactory('solarpi.lan')
     #this currently does not work in older docker versions:
     #factory = PiGPIOFactory('host.docker.internal')
 
-pin_moisture_sensor = 20
-pin_water_solenoid = 21
-pin_5v_solenoid = 26
+#pin_moisture_sensor = 20
+#pin_water_solenoid = 21
+#pin_5v_solenoid = 26
 
 class water_pump(object):
     def __init__(self):
         self.state = 0
         g_settings = config.loadSettings()
-        self.solenoid = gpiozero.LED(g_settings.hardware_water_gpio_pin, pin_factory=factory)
+        self.solenoid = gpiozero.LED(int(g_settings.hardware_water_gpio_pin), pin_factory=factory)
         self.solenoid.off()
         
     def On(self):
         self.state = 1
         g_settings = config.loadSettings()
-        self.solenoid = gpiozero.LED(g_settings.hardware_water_gpio_pin, pin_factory=factory)
+        self.solenoid = gpiozero.LED(int(g_settings.hardware_water_gpio_pin), pin_factory=factory)
         self.solenoid.on()
         
     def Off(self):
         self.state = 0
         g_settings = config.loadSettings()
-        self.solenoid = gpiozero.LED(g_settings.hardware_water_gpio_pin, pin_factory=factory)
+        self.solenoid = gpiozero.LED(int(g_settings.hardware_water_gpio_pin), pin_factory=factory)
 
         self.solenoid.off()
                     
     def water_for(self, secs):
             g_settings = config.loadSettings()
             self.state = 1
-            self.solenoid = gpiozero.LED(g_settings.hardware_water_gpio_pin, pin_factory=factory)
+            self.solenoid = gpiozero.LED(int(g_settings.hardware_water_gpio_pin), pin_factory=factory)
             self.solenoid.on()
             time.sleep(secs)
             self.solenoid.off()
